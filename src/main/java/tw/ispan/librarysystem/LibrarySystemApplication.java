09fc72746dccba306f2bd3ce8bfe2abb2ed0b71c
bhuan <bhuang517@gmail.com>
2025-05-19 09:04:16 +0800

    first commit

--- src/main/java/tw/ispan/librarysystem/LibrarySystemApplication.java
+++ src/main/java/tw/ispan/librarysystem/LibrarySystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibrarySystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibrarySystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemApplication.class, args);
+    }
 
 }
