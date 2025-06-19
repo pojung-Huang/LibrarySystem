c0f20efbba655407af0dcfbc65c199d2b6d884f9
bhuan <bhuang517@gmail.com>
2025-06-19 22:50:48 +0800

    add Manager Member DTO

---
+++ src/main/java/tw/ispan/librarysystem/dto/manager/accounts/ManagerMemberDTO.java
@@ -0,0 +1,25 @@
+package tw.ispan.librarysystem.dto.manager.accounts;
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+public class ManagerMemberDTO {
+    private Integer id;
+    private String name;
+    private String gender;
+    private String idNumber;
+    private LocalDate birthDate;
+    private String nationality;
+    private String education;
+    private String occupation;
+    private String addressCounty;
+    private String addressTown;
+    private String addressZip;
+    private String addressDetail;
+    private String email;
+    private String phone;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
